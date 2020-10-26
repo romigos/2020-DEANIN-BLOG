145a84cca3748f15c8aaa03007e4715f2276fe50
romigos <bolistovskiyroman@gmail.com>
2020-10-26 17:06:58 +0200

    create controller about

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap'
+import './src/application.scss'
+
 globsl.$ - jQuery;
